9ac0c6a9e8fb38babde36c082d2c5c5d3c80740f
indransh-0804 <indransh.0804.sharma@gmail.com>
2026-06-25 23:58:49 +0530

    more minor fuckups

--- modules/system/nix/nh.nix
+++ modules/system/nix/nh.nix
@@ -9,5 +9,9 @@ flake.nixosModules.nix = {
       extraArgs = "--keep-since 21d --keep 3";
     };
   };
+  environment.variables = {
+  NH_FLAKE = "/home/indransh/.nixos";
+  NH_OS_FLAKE = "/home/indransh/.nixos";
+  };
 };
 }
--- modules/system/services/torrent.nix
+++ modules/system/services/torrent.nix
@@ -1,15 +1,11 @@
 {inputs, ...}:{
 flake.nixosModules.torrent = { config, pkgs, ... }:
-
 {
-  # 1. Create a dedicated 'media' group
-  users.groups.media = {};
+  users = {
+  groups.media = {};
+  users.indransh.extraGroups = [ "media" ];
+  };
 
-  # 2. Add your personal user to the media group so you can manage the files
-  # REPLACE "myuser" WITH YOUR ACTUAL NIXOS USERNAME
-  users.users.indransh.extraGroups = [ "media" ];
-
-  # 3. Create the directories automatically with the correct permissions
   systemd.tmpfiles.rules = [
     "d /data/media 2775 root media - -"
     "d /data/media/tv 2775 root media - -"
@@ -17,33 +13,27 @@ flake.nixosModules.torrent = { config, pkgs, ... }:
     "d /data/media/downloads 2775 root media - -"
   ];
 
-  # 4. Enable the Services
   services.sonarr = {
     enable = true;
     openFirewall = true;
+    group = "media"; # Forces Sonarr to run under the media group
   };
 
   services.radarr = {
     enable = true;
     openFirewall = true;
+    group = "media"; 
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    group = "media"; 
   };
 
   services.prowlarr = {
     enable = true;
     openFirewall = true;
   };
-
-  # Note: The native qbittorrent service is available in NixOS 24.05 and newer.
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  # 5. Add the automatically created service users to the 'media' group
-  # This ensures they can all read and write to /data/media without conflicts
-  users.users.sonarr.extraGroups = [ "media" ];
-  users.users.radarr.extraGroups = [ "media" ];
-  users.users.prowlarr.extraGroups = [ "media" ];
-  users.users.qbittorrent.extraGroups = [ "media" ];
-};
+}
 }
